6b741ab80a9fb05cc81bc248ad54e3d3da7c31d2
edy4c7 <edy4c7@gmail.com>
2020-11-28 16:02:19 +0900

    setup firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
 import * as functions from 'firebase-functions';
+import express from 'express'
+//@ts-ignore  
+import basicAuth from 'basic-auth-connect'
+import { compareSync } from 'bcrypt'
+import { join } from 'path'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.all('/*', basicAuth((user: string, password: string) => {
+  return user === functions.config().basic.user
+  && compareSync(password, functions.config().basic.password)
+}))
+
+app.use(express.static(join(__dirname, '../web/dist')))
+
+exports.app = functions.https.onRequest(app)
