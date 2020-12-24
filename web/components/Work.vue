898edf34dbe2bda2bd7a3fcaf6a8b4bca992cfdd
edy4c7 <edy4c7@gmail.com>
2020-12-24 22:30:37 +0900

    update Work.vue

--- web/components/Work.vue
+++ web/components/Work.vue
@@ -1,12 +1,38 @@
 <template>
   <v-card>
-    <v-img :src="require('~/assets/image.jpg')" :contains="true" />
-    <v-card-title>Title</v-card-title>
-    <v-card-subtitle>Author</v-card-subtitle>
+    <v-img :src="content.thumbnailUrl" :contains="true" />
+    <v-card-title>{{ content.title }}</v-card-title>
+    <v-card-subtitle>{{ content.author }}</v-card-subtitle>
     <v-card-text>
-      caption caption caption caption caption caption caption caption caption
-      caption caption caption caption caption caption caption caption captio
-      caption caption caption caption caption caption caption caption caption
+      <p class="description">
+        {{ content.description }}
+      </p>
+      <p class="link">
+        <v-btn :href="content.contentUrl" target="_blank"
+          >{{ domainName }}
+        </v-btn>
+      </p>
     </v-card-text>
   </v-card>
 </template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+import { Work } from '~/plugins/api'
+
+export default defineComponent({
+  props: {
+    content: {
+      type: Object as PropType<Work>,
+      required: true,
+    },
+  },
+  setup(props, _) {
+    const domainName = props.content.contentUrl.split('/')[2]
+
+    return {
+      domainName,
+    }
+  },
+})
+</script>
---
+++ web/test/components/work.test.ts
@@ -0,0 +1,76 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Work from '~/components/Work.vue'
+import { Work as IWork } from '~/plugins/api'
+
+Vue.use(Vuetify)
+
+const localVue = createLocalVue()
+
+const content: IWork = {
+  id: '01',
+  author: 'taro',
+  title: 'hoge',
+  description: 'aaaaaaaaaaaaaaaa',
+  thumbnailUrl: 'https://example.com/thumbnail',
+  contentUrl: 'https://example.com',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+
+describe('Work', () => {
+  let vuetify: Vuetify
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('show content title', () => {
+    const wrapper = shallowMount(Work, {
+      localVue,
+      vuetify,
+      propsData: {
+        content,
+      },
+    })
+
+    expect(wrapper.html()).toContain(content.title)
+  })
+
+  it('show author of content ', () => {
+    const wrapper = shallowMount(Work, {
+      localVue,
+      vuetify,
+      propsData: {
+        content,
+      },
+    })
+
+    expect(wrapper.html()).toContain(content.author)
+  })
+
+  it('show description of content ', () => {
+    const wrapper = shallowMount(Work, {
+      localVue,
+      vuetify,
+      propsData: {
+        content,
+      },
+    })
+
+    expect(wrapper.html()).toContain(content.description)
+  })
+
+  it('link to content ', () => {
+    const wrapper = shallowMount(Work, {
+      localVue,
+      vuetify,
+      propsData: {
+        content,
+      },
+    })
+
+    expect(wrapper.html()).toContain(`href="${content.contentUrl}"`)
+    expect(wrapper.text()).toContain('example.com')
+  })
+})
