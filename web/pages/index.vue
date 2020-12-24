b485a1bb3a3ec255c1201611c5ced4bd426c36bc
edy4c7 <edy4c7@gmail.com>
2020-12-24 23:17:41 +0900

    update pages/index.vue(add seek buttons)

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -58,8 +58,28 @@
         </v-col>
       </v-row>
     </v-container>
-    <modal :is-visible.sync="isWorkModalVisible">
-      <work class="work" />
+    <modal :is-visible.sync="localState.isWorkModalVisible">
+      <v-btn
+        v-if="currentIndex > 0"
+        slot="previousButton"
+        icon
+        large
+        dark
+        @click="seek(-1)"
+      >
+        <v-icon> mdi-chevron-left </v-icon>
+      </v-btn>
+      <work slot="content" class="work" :content="localState.modalContent" />
+      <v-btn
+        v-if="currentIndex < countOfItems - 1"
+        slot="nextButton"
+        icon
+        large
+        dark
+        @click="seek(1)"
+      >
+        <v-icon> mdi-chevron-right </v-icon>
+      </v-btn>
     </modal>
   </div>
 </template>
@@ -92,47 +112,56 @@
 import {
   computed,
   defineComponent,
-  ref,
-  useContext,
-  useFetch,
+  getCurrentInstance,
+  reactive,
 } from '@nuxtjs/composition-api'
-import { Store } from 'vuex'
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import Work from '~/components/Work.vue'
+import { Work as IWork } from '~/plugins/api'
 import Modal from '~/components/Modal.vue'
-import { State } from '~/store'
-
-function useStore<S>() {
-  const { store } = useContext()
-  return store as Store<S>
-}
 
 export default defineComponent({
   components: {
-    Logo,
-    VuetifyLogo,
     Work,
     Modal,
   },
-
   setup() {
-    const store = useStore<State>()
-    const isWorkModalVisible = ref<Boolean>(false)
-    const items = computed(() => store.state.works)
-
-    useFetch(async () => {
-      await store.dispatch('fetchWorks')
+    const self = getCurrentInstance()
+    const items = computed(() => self?.$store.state.works)
+    const currentIndex = computed(() =>
+      self?.$store.state.works.indexOf(localState.modalContent)
+    )
+    const countOfItems = computed(() => self?.$store.state.works.length)
+    const localState = reactive({
+      isWorkModalVisible: false,
+      modalContent: {
+        id: '',
+        author: '',
+        title: '',
+        description: '',
+        thumbnailUrl: '',
+        contentUrl: '',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     })
 
-    function showWorkModal() {
-      isWorkModalVisible.value = true
+    function showWorkModal(content: IWork) {
+      localState.modalContent = content
+      localState.isWorkModalVisible = true
+    }
+
+    function seek(delta: number) {
+      localState.modalContent =
+        self?.$store.state.works[currentIndex.value + delta]
     }
 
     return {
       items,
-      isWorkModalVisible,
+      currentIndex,
+      countOfItems,
+      localState,
       showWorkModal,
+      seek,
     }
   },
 })
