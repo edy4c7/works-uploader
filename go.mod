3e8428ea2d6699f3267ddac80038b8f3d54fa082
edy4c7 <edy4c7@gmail.com>
2021-06-05 11:47:14 +0900

    Add message printer

--- go.mod
+++ go.mod
@@ -19,7 +19,7 @@ require (
 	github.com/ugorji/go v1.2.2 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/text v0.3.5
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- internal/middlewares/error_middleware.go
+++ internal/middlewares/error_middleware.go
@@ -5,30 +5,35 @@ import (
 	"net/http"
 	"strings"
 
-	myErr "github.com/edy4c7/works-uploader/internal/errors"
+	"github.com/edy4c7/works-uploader/internal/beans"
+	wuErr "github.com/edy4c7/works-uploader/internal/errors"
 	"github.com/edy4c7/works-uploader/internal/i18n"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
-func NewErrorMiddleware(messageLoader i18n.MessageLoader) gin.HandlerFunc {
+var mapStatusCode = map[string]int{
+	wuErr.WUE01: http.StatusNotFound,
+	wuErr.WUE02: http.StatusForbidden,
+	wuErr.WUE99: http.StatusInternalServerError,
+}
+
+func NewErrorMiddleware(messagePrinter i18n.Printer) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 
 		if err := c.Errors.Last(); err != nil {
-			var appErr *myErr.ApplicationError
-			if errors.As(err.Err, appErr) {
-				wrapedErr := myErr.NewApplicationError(
-					myErr.Message(messageLoader.LoadMessage(appErr.Code(), "ja-JP", appErr.MessageParams())),
-					myErr.Code(appErr.Code()),
-					myErr.Cause(appErr),
-				)
-
-				switch appErr.Code() {
-				case myErr.WUE01:
-					c.AbortWithError(http.StatusNotFound, wrapedErr)
-				case myErr.WUE99:
-					c.AbortWithError(http.StatusInternalServerError, wrapedErr)
+			var appErr *wuErr.ApplicationError
+			if errors.As(err.Err, &appErr) {
+				lang := c.Request.Header.Get("Accept-Language")
+				errBean := &beans.ErrorBean{
+					Code:    appErr.Code(),
+					Message: messagePrinter.Print(lang, appErr.Code(), appErr.MessageParams()),
+				}
+				if sc, ok := mapStatusCode[appErr.Code()]; ok {
+					c.AbortWithStatusJSON(sc, errBean)
+				} else {
+					c.AbortWithStatusJSON(http.StatusInternalServerError, errBean)
 				}
 			}
 		}
@@ -45,11 +50,11 @@ func NewValidationErrorHandler() gin.HandlerFunc {
 				fe := ve[0]
 
 				c.AbortWithError(http.StatusBadRequest,
-					myErr.NewApplicationError(
-						myErr.Code(fe.Tag()),
-						myErr.Cause(fe),
-						myErr.Message("Validation Error"),
-						myErr.MessageParams(fe.Namespace(), fe.Field(), strings.Split(fe.Param(), ",")),
+					wuErr.NewApplicationError(
+						wuErr.Code(fe.Tag()),
+						wuErr.Cause(fe),
+						wuErr.Message("Validation Error"),
+						wuErr.MessageParams(fe.Namespace(), fe.Field(), strings.Split(fe.Param(), ",")),
 					),
 				)
 			}
